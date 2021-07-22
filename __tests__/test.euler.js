e75a4fd16751c91071a1e73e7b70613fe589d00e
shanole <73411129+shanole@users.noreply.github.com>
2021-07-21 20:46:35 -0700

    working solution for project euler
    Co-Authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- __tests__/test.euler.js
+++ __tests__/test.euler.js
@@ -3,3 +3,23 @@ import { euler } from './../src/euler.js'
 test('should return 0 if argument <= 3', () => {
   expect(euler(3)).toEqual(0);
 })
+
+test('should return 3 if argument = 4', () => {
+  expect(euler(4)).toEqual(3);
+})
+
+test('should return 3 if argument = 5', () => {
+  expect(euler(5)).toEqual(3);
+})
+
+test('should return (3+5) if argument = 6', () => {
+  expect(euler(6)).toEqual(8);
+})
+
+test('should return 23 if argument = 10', () => {
+  expect(euler(10)).toEqual(23);
+})
+
+test('should return 233168 if argument = 1000', () => {
+  expect(euler(1000)).toEqual(233168);
+})
--- src/euler.js
+++ src/euler.js
@@ -1,4 +1,26 @@
-export const euler = (num) =>
-{
-  return;
+export const euler = (num) => {
+    if (num <= 3) {
+        return 0;
+    } else {
+        let newNum = num-1;
+        if ((newNum % 3 === 0) || (newNum % 5 === 0)) {
+            return euler(newNum) + newNum;
+        } else {
+            return euler(newNum);
+        }
+    }
 }
+
+// const euler = (num) => {
+//   if (num <= 3) {
+//     return 0; 
+//   }
+//   else {
+//     let newNum = num-1;
+//     let numToAdd = 0;
+//     if ( newNum % 3 || newNum % 5) {
+//       numToAdd = newNum
+//     }
+//     return euler(newNum) + newNum;
+//   }
+// }
